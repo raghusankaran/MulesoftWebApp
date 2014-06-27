8b5f2204903ccefb06fb808164fb5badf0198306
Naseem Al-Naji <naseem.alnaji@ML-NALNAJI-OSX-SF.local>
2014-06-27 16:51:08 -0700

    fixed global names

--- config.js
+++ config.js
@@ -1,18 +1,10 @@
 var urlPath = "http://mule-perflab06.managed.contegix.com:8880/";
 var hudsonPath = "/home/perftest/.hudson/jobs";
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//url to our app
-module.exports.urlPath = urlPath;
-
-//path to hudson's jobs directory
-module.exports.hudsonPath = hudsonPath
-
-//Get job names
-module.exports.retrieveJobs = urlPath + 'retrieveJobs/';
-
-//get build names
-module.exports.retrieveBuilds = urlPath + 'retrieveBuilds/?build=';
-
-//get data
-module.exports.retrieveData = urlPath + 'data/';
+var config = {
+	url: "http://mule-perflab06.managed.contegix.com:8880/",
+	hudsonPath: "/home/perftest/.hudson/jobs",
+	retrieveJobs: urlPath+ 'retrieveJobs/',
+	retrieveBuilds: urlPath + 'retrieveBuilds/?build=',
+	retrieveData: urlPath + 'data/'
+};
