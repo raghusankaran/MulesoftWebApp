c7d98b2d081e18fa1b578025f7e7de61d200e4f5
Naseem Al-Naji <naseem.alnaji@ML-NALNAJI-OSX-SF.local>
2014-06-27 17:11:08 -0700

    fixing job paths

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var hudsonPath1 = "/home/perftest/.hudson/jobs";
 
 var config = {
 	url: "http://mule-perflab06.managed.contegix.com:8880/",
-	hudsonPath: "jobs/",
+	hudsonPath: "/home/perftest/.hudson/jobs/",
 	retrieveJobs: urlPath+ 'retrieveJobs/',
 	retrieveBuilds: urlPath + 'retrieveBuilds/?build=',
 	retrieveData: urlPath + 'data/'
