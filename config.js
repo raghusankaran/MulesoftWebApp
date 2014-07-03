99eed7239486d7260f8d6a586d481e2c72fccb06
Naseem Al-Naji <naseem.alnaji@ML-NALNAJI-OSX-SF.local>
2014-07-02 22:50:14 -0700

    adding other builds area

--- config.js
+++ config.js
@@ -15,19 +15,3 @@ var config = {
 };
 
 module.exports.config = config;
-
-
-
-
-// var urlPath = "http://mule-perflab06.managed.contegix.com:8880/";
-// var hudsonPath1 = "/home/perftest/.hudson/jobs";
-// var config = {
-// 	url: "http://mule-perflab06.managed.contegix.com:8880/",
-// 	hudsonPath: "/home/perftest/.hudson/jobs/",
-// 	retrieveJobs: urlPath+ 'retrieveJobs/',
-// 	retrieveBuilds: urlPath + 'retrieveBuilds/?build=',
-// 	retrieveData: urlPath + 'data/',
-// 	jobHost: 'http://mule-perflab06.managed.contegix.com:8080/api/json'
-// };
-
-// module.exports.config = config;
