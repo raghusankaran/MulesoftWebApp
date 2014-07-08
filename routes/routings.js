182dffbb1612b9dc936a1ec05214295af00a1985
Naseem Al-Naji <naseem.alnaji@ML-NALNAJI-OSX-SF.local>
2014-07-07 17:21:11 -0700

    fixed error with headers

--- routes/routings.js
+++ routes/routings.js
@@ -153,270 +153,271 @@ exports.getData = function(req, res){
 				if(relative == 'ERROR'){
 					res.send('Bad build');
 				}
-
-				path+=relative;
-				//
-				if(type.indexOf('cpu') >=0)
-				{	
-					var nameOfFile = 'sar.cpuusage.out';
-					//
-					if(type.indexOf('use') >=0){	
-						fs.readFile(path+nameOfFile, {encoding: 'utf-8'}, function(err, str)
-						{
-							
-							var locateStart = str.indexOf('%idle');
-							str = str.substring(locateStart+5);
-							var dataSet = str.match(/\s\s\d+\.\d+/g);
-							//usr + nice + sys + irq 
-							var data = '';
-							
-							var jump = 9;
-							for(var i = 0; i < dataSet.length; i+= jump)
-							{
-								data += (parseFloat(dataSet[i]) + parseFloat(dataSet[i+1]) + parseFloat(dataSet[i+2]) + parseFloat(dataSet[i+5]))+',';
-							}
-							res.send(data);
-						});					
-					}
-					else if(type.indexOf('iowait') >=0){
-						fs.readFile(path+nameOfFile, {encoding: 'utf-8'}, function(err, str)
-						{
-							
-							var locateStart = str.indexOf('%idle');
-							str = str.substring(locateStart+5);
-							var dataSet = str.match(/\s\s\d+\.\d+/g);
-							//usr + nice + sys + irq 
-							var data = '';
-							
-							var jump = 9;
-							for(var i = 0; i < dataSet.length; i+= jump)
-							{
-								data += dataSet[i+3]+',';
-							}
-							res.send(data);
-						});	
-
-					}
-					else if(type.indexOf('steal') >=0){
-						fs.readFile(path+nameOfFile, {encoding: 'utf-8'}, function(err, str)
-						{
-							
-							var locateStart = str.indexOf('%idle');
-							str = str.substring(locateStart+5);
-							var dataSet = str.match(/\s\s\d+\.\d+/g);
-							//usr + nice + sys + irq 
-							var data = '';
-							
-							var jump = 9;
-							for(var i = 0; i < dataSet.length; i+= jump)
-							{
-								data += dataSet[i+4]+',';
-							}
-							res.send(data);
-						});	
-					}
-				}
-				else if(type.indexOf('mem') >=0)
-				{	
-					var nameOfFile = 'sar.memutil.out';
-					if(type.indexOf('use') >=0){
-						
-						//
-						fs.readFile(path+nameOfFile, {encoding: 'utf-8'}, function(err, str)
-						{
-							
-							var locateStart = str.indexOf('%commit');
-							str = str.substring(locateStart+7);
-							var dataSet = str.match(/\s\s\d+(\.)?\d+/g);
-							//usr + nice + sys + irq 
-							var data = '';
-							
-							var jump = 7;
-							for(var i = 0; i < dataSet.length; i+= jump)
-							{
-								data += dataSet[i+2]+',';
-							}
-							res.send(data);
-						});	
-					}
-					else if(type.indexOf('kbcache') >=0){
-						fs.readFile(path+nameOfFile, {encoding: 'utf-8'}, function(err, str)
-						{
-							
-							var locateStart = str.indexOf('%commit');
-							str = str.substring(locateStart+7);
-							var dataSet = str.match(/\s\s\d+(\.)?\d+/g);
-							//usr + nice + sys + irq 
-							var data = '';
-							
-							var jump = 7;
-							for(var i = 0; i < dataSet.length; i+= jump)
-							{
-								data += dataSet[i+4]+',';
-							}
-							res.send(data);
-						});
-					}
-					else if(type.indexOf('kbbuffers') >=0){
-						fs.readFile(path+nameOfFile, {encoding: 'utf-8'}, function(err, str)
-						{
-							
-							var locateStart = str.indexOf('%commit');
-							str = str.substring(locateStart+7);
-							var dataSet = str.match(/\s\s\d+(\.)?\d+/g);
-							//usr + nice + sys + irq 
-							var data = '';
-							
-							var jump = 7;
-							for(var i = 0; i < dataSet.length; i+= jump)
-							{
-								data += dataSet[i+3]+',';
-							}
-							res.send(data);
-						});
-					}						
-				}
-
-				else if(type.indexOf('disk') >=0){
-					var nameOfFile = 'sar.device.out';
-					
-					fs.readFile(path + nameOfFile, {encoding: 'utf-8'}, function(err, str)
-					{			
-						var temp = str.match(/\s\d+\.\d\d/g);
-						var data = '';
-						if(type.indexOf('util') >=0){
-							for(var i = 0; i < temp.length; i++){
-								if((i+1) % 8 == 0){
-									data += temp[i]+',';
-								}
-							}
-						}
-
-						else if(type.indexOf('await') >=0){
-							for(var i = 0; i < temp.length; i++){
-								if((i+1) % 8 == 6){
-									data += temp[i]+',';
-								}
-							}
-						}
-						else if(type.indexOf('tps') >=0){
-							for(var i = 0; i < temp.length; i++){
-								if((i+1) % 8 == 1){
-									data += temp[i]+',';
-								}
-							}
-
-						}
-						else if(type.indexOf('wr_sec') >=0){
-							
-							for(var i = 0; i < temp.length; i++){
-								if((i+1 )% 8 == 3){
-									data += temp[i]+',';
-								}
-							}
-						}
-						else if(type.indexOf('rd_sec') >=0){
-							
-							for(var i = 0; i < temp.length; i++){
-								if((i+1) % 8 == 2){
-									data += temp[i]+',';
-								}
-							}
-						}
-						res.send(data);
-
-					});
-
-					
-				}
-
-				else if(type.indexOf('network') >=0){
-					var nameOfFile = 'sar.network.DEV.out';
-					fs.readFile(path + nameOfFile, {encoding: 'utf-8'}, function(err, str)
-					{			
-						var temp = str.split(/[^\w)\d]\n/g);
-						var subgroups = [];
-						temp.splice(0,1);
-						var sections = temp.length;
-						for(var i = 0; i < temp.length; i++){
-							subgroups[subgroups.length] = temp[i].match(/\s\s\d+\.\d+/g);
-						}
-						var maxTString = '';
-						var maxRString = '';
-						var len = subgroups[0].length/7;
-						
-						temp = str.match(/\s\s\d+\.\d+/g);
-						var maxT = 0, maxR=0;
-						var dataT = [], dataR = [];
-						for(var i = 0; i <  temp.length; i++){
-							if((i+1) % 7 == 4)
-							{
-								dataT[dataT.length] = temp[i];
-							}
-							else if((i+1) % 7 == 3 ){
-								dataR[dataR.length] = temp[i];
-							}
-						}
-						var maxRSet = [];
-						var maxTSet = [];
-						var count = 0;
-						for(var k = 0; k < dataT.length; k++){
-							if(count == len ){
-								maxTSet[maxTSet.length] = ''+ maxT;
-								maxTString += maxT +',';
-								maxT = 0;
-								count = 0;
-							}
-							
-							if(maxT < dataT[k]){
-								maxT = dataT[k];	
-							}
-							count++;
-							
-						}
-						count = 0;
-						for(var k = 0; k < dataR.length; k++){
-							if(count == len ){
-								maxRSet[maxRSet.length] = maxR;
-								maxRString += maxR +',';
-								maxR = 0;
-								count = 0;
-							}
-							
-							if(maxR < dataR[k]){
-								maxR = dataR[k];	
-							}
-							count++;
-							
-						}
-						var data = '';
-						if(type.indexOf('used') >=0){
-							data = '';
-							for(var i = 0; i < maxRSet.length; i++){
-								var temp = Math.max(maxRSet[i], maxTSet[i]);
-								temp = (((temp * 8 ) / 1024 )/ 102.4);
-								data += temp + ',';
-							}
-							res.send(data);
-						}
-						else if(type.indexOf('txkB') >=0){
-							res.send(maxTString);
-						}
-						else if(type.indexOf('rxkB') >=0){
-							res.send(maxRString);
-						}
-						
-					});
-					
-				}
-
-				else if(type.indexOf('duration') >=0){
-					res.send(''+ info.duration);
-				}
-
 				else{
-					res.send('');
-					
+					path+=relative;
+					//
+					if(type.indexOf('cpu') >=0)
+					{	
+						var nameOfFile = 'sar.cpuusage.out';
+						//
+						if(type.indexOf('use') >=0){	
+							fs.readFile(path+nameOfFile, {encoding: 'utf-8'}, function(err, str)
+							{
+								
+								var locateStart = str.indexOf('%idle');
+								str = str.substring(locateStart+5);
+								var dataSet = str.match(/\s\s\d+\.\d+/g);
+								//usr + nice + sys + irq 
+								var data = '';
+								
+								var jump = 9;
+								for(var i = 0; i < dataSet.length; i+= jump)
+								{
+									data += (parseFloat(dataSet[i]) + parseFloat(dataSet[i+1]) + parseFloat(dataSet[i+2]) + parseFloat(dataSet[i+5]))+',';
+								}
+								res.send(data);
+							});					
+						}
+						else if(type.indexOf('iowait') >=0){
+							fs.readFile(path+nameOfFile, {encoding: 'utf-8'}, function(err, str)
+							{
+								
+								var locateStart = str.indexOf('%idle');
+								str = str.substring(locateStart+5);
+								var dataSet = str.match(/\s\s\d+\.\d+/g);
+								//usr + nice + sys + irq 
+								var data = '';
+								
+								var jump = 9;
+								for(var i = 0; i < dataSet.length; i+= jump)
+								{
+									data += dataSet[i+3]+',';
+								}
+								res.send(data);
+							});	
+
+						}
+						else if(type.indexOf('steal') >=0){
+							fs.readFile(path+nameOfFile, {encoding: 'utf-8'}, function(err, str)
+							{
+								
+								var locateStart = str.indexOf('%idle');
+								str = str.substring(locateStart+5);
+								var dataSet = str.match(/\s\s\d+\.\d+/g);
+								//usr + nice + sys + irq 
+								var data = '';
+								
+								var jump = 9;
+								for(var i = 0; i < dataSet.length; i+= jump)
+								{
+									data += dataSet[i+4]+',';
+								}
+								res.send(data);
+							});	
+						}
+					}
+					else if(type.indexOf('mem') >=0)
+					{	
+						var nameOfFile = 'sar.memutil.out';
+						if(type.indexOf('use') >=0){
+							
+							//
+							fs.readFile(path+nameOfFile, {encoding: 'utf-8'}, function(err, str)
+							{
+								
+								var locateStart = str.indexOf('%commit');
+								str = str.substring(locateStart+7);
+								var dataSet = str.match(/\s\s\d+(\.)?\d+/g);
+								//usr + nice + sys + irq 
+								var data = '';
+								
+								var jump = 7;
+								for(var i = 0; i < dataSet.length; i+= jump)
+								{
+									data += dataSet[i+2]+',';
+								}
+								res.send(data);
+							});	
+						}
+						else if(type.indexOf('kbcache') >=0){
+							fs.readFile(path+nameOfFile, {encoding: 'utf-8'}, function(err, str)
+							{
+								
+								var locateStart = str.indexOf('%commit');
+								str = str.substring(locateStart+7);
+								var dataSet = str.match(/\s\s\d+(\.)?\d+/g);
+								//usr + nice + sys + irq 
+								var data = '';
+								
+								var jump = 7;
+								for(var i = 0; i < dataSet.length; i+= jump)
+								{
+									data += dataSet[i+4]+',';
+								}
+								res.send(data);
+							});
+						}
+						else if(type.indexOf('kbbuffers') >=0){
+							fs.readFile(path+nameOfFile, {encoding: 'utf-8'}, function(err, str)
+							{
+								
+								var locateStart = str.indexOf('%commit');
+								str = str.substring(locateStart+7);
+								var dataSet = str.match(/\s\s\d+(\.)?\d+/g);
+								//usr + nice + sys + irq 
+								var data = '';
+								
+								var jump = 7;
+								for(var i = 0; i < dataSet.length; i+= jump)
+								{
+									data += dataSet[i+3]+',';
+								}
+								res.send(data);
+							});
+						}						
+					}
+
+					else if(type.indexOf('disk') >=0){
+						var nameOfFile = 'sar.device.out';
+						
+						fs.readFile(path + nameOfFile, {encoding: 'utf-8'}, function(err, str)
+						{			
+							var temp = str.match(/\s\d+\.\d\d/g);
+							var data = '';
+							if(type.indexOf('util') >=0){
+								for(var i = 0; i < temp.length; i++){
+									if((i+1) % 8 == 0){
+										data += temp[i]+',';
+									}
+								}
+							}
+
+							else if(type.indexOf('await') >=0){
+								for(var i = 0; i < temp.length; i++){
+									if((i+1) % 8 == 6){
+										data += temp[i]+',';
+									}
+								}
+							}
+							else if(type.indexOf('tps') >=0){
+								for(var i = 0; i < temp.length; i++){
+									if((i+1) % 8 == 1){
+										data += temp[i]+',';
+									}
+								}
+
+							}
+							else if(type.indexOf('wr_sec') >=0){
+								
+								for(var i = 0; i < temp.length; i++){
+									if((i+1 )% 8 == 3){
+										data += temp[i]+',';
+									}
+								}
+							}
+							else if(type.indexOf('rd_sec') >=0){
+								
+								for(var i = 0; i < temp.length; i++){
+									if((i+1) % 8 == 2){
+										data += temp[i]+',';
+									}
+								}
+							}
+							res.send(data);
+
+						});
+
+						
+					}
+
+					else if(type.indexOf('network') >=0){
+						var nameOfFile = 'sar.network.DEV.out';
+						fs.readFile(path + nameOfFile, {encoding: 'utf-8'}, function(err, str)
+						{			
+							var temp = str.split(/[^\w)\d]\n/g);
+							var subgroups = [];
+							temp.splice(0,1);
+							var sections = temp.length;
+							for(var i = 0; i < temp.length; i++){
+								subgroups[subgroups.length] = temp[i].match(/\s\s\d+\.\d+/g);
+							}
+							var maxTString = '';
+							var maxRString = '';
+							var len = subgroups[0].length/7;
+							
+							temp = str.match(/\s\s\d+\.\d+/g);
+							var maxT = 0, maxR=0;
+							var dataT = [], dataR = [];
+							for(var i = 0; i <  temp.length; i++){
+								if((i+1) % 7 == 4)
+								{
+									dataT[dataT.length] = temp[i];
+								}
+								else if((i+1) % 7 == 3 ){
+									dataR[dataR.length] = temp[i];
+								}
+							}
+							var maxRSet = [];
+							var maxTSet = [];
+							var count = 0;
+							for(var k = 0; k < dataT.length; k++){
+								if(count == len ){
+									maxTSet[maxTSet.length] = ''+ maxT;
+									maxTString += maxT +',';
+									maxT = 0;
+									count = 0;
+								}
+								
+								if(maxT < dataT[k]){
+									maxT = dataT[k];	
+								}
+								count++;
+								
+							}
+							count = 0;
+							for(var k = 0; k < dataR.length; k++){
+								if(count == len ){
+									maxRSet[maxRSet.length] = maxR;
+									maxRString += maxR +',';
+									maxR = 0;
+									count = 0;
+								}
+								
+								if(maxR < dataR[k]){
+									maxR = dataR[k];	
+								}
+								count++;
+								
+							}
+							var data = '';
+							if(type.indexOf('used') >=0){
+								data = '';
+								for(var i = 0; i < maxRSet.length; i++){
+									var temp = Math.max(maxRSet[i], maxTSet[i]);
+									temp = (((temp * 8 ) / 1024 )/ 102.4);
+									data += temp + ',';
+								}
+								res.send(data);
+							}
+							else if(type.indexOf('txkB') >=0){
+								res.send(maxTString);
+							}
+							else if(type.indexOf('rxkB') >=0){
+								res.send(maxRString);
+							}
+							
+						});
+						
+					}
+
+					else if(type.indexOf('duration') >=0){
+						res.send(''+ info.duration);
+					}
+
+					else{
+						res.send('');
+						
+					}
 				}
 			}
 	});
