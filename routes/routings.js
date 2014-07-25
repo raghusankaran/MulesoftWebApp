46d946d17511be09ad1b34133db5264a814dd1b5
FratNaji <fratnaji@ML-NALNAJI-OSX-SF.local>
2014-07-24 17:55:47 -0700

    adding perfci updates...

--- routes/routings.js
+++ routes/routings.js
@@ -461,7 +461,7 @@ function getAllPerfCIJobs(id){
 
 				for(var i=0; i<listOfFilenames.length; i++){
 					console.log(listOfFilenames[i]);
-					result += config.hudsonPath+'PERF_CI/builds/'+fileID+'/archive/logs/' +listOfFilenames[i] + '/parsed,';
+					result += config.hudsonPath+'PERF_CI/builds/'+fileID+'/archive/logs/' +listOfFilenames[i] + '/parsed/,';
 				}
 				console.log(result);
 								
