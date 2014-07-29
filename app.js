bf5f61fa9263275d1ee91ea3eea4fe08c7d0442d
FratNaji <fratnaji@ML-NALNAJI-OSX-SF.local>
2014-07-28 17:59:18 -0700

    adding login and security

--- app.js
+++ app.js
@@ -36,7 +36,8 @@ app.post('/login', function (req, res) {
     req.session.user_id = 'john';
     res.redirect('/');
   } else {
-    res.send('Bad user/pass');
+    
+    res.redirect('/');
   }
 });
 
