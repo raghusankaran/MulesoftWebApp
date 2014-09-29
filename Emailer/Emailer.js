abc825c32480503a145f293474dbb677254a6698
nalnaji <fratnaji@ML-NALNAJI-OSX-SF.local>
2014-09-29 00:36:56 -0700

    Added new elements to the report

--- Emailer/Emailer.js
+++ Emailer/Emailer.js
@@ -1,18 +1,14 @@
 var fs = require('fs');
 var request = require('request');
-
 var username = "naseem.alnaji";
 var password = "1325842Mulesoft";
 var auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
-
 function newBuildExists(){
 	//Check for new builds
 	var url = "http://mule-perflab06.managed.contegix.com:8080/job/PERF_CI/api/json?tree=builds[number,url]";
 	var url = "http://mule-perflab06.managed.contegix.com:8080/job/PERF_CI/api/json?tree=lastStableBuild[number,url]";
 	return new Promise(function (fulfill, reject){
-
 		getRequestWithAuth(url, username, password).then(function(res){
-
 			var builds = res['lastStableBuild'];			
 			var current = fs.readFileSync('history.txt',encoding='utf8'); //Get current by accessing filesystem
 			console.log('History.txt reads: '+ current);
@@ -24,14 +20,9 @@ function newBuildExists(){
 			else
 				fulfill(false);
 		});
-	});
-		
-
-	
+	});	
 }
-
 var nodemailer = require('nodemailer');
-
 // create reusable transporter object using SMTP transport
 var transporter = nodemailer.createTransport({
     service: 'Gmail',
@@ -40,7 +31,6 @@ var transporter = nodemailer.createTransport({
         pass: 'anypoint'
     }
 });
-
 function getRequest(urlDest){
 	return new Promise(function (fulfill, reject){
 		request(
@@ -48,9 +38,7 @@ function getRequest(urlDest){
 	        url : urlDest	        
 	    },
 		    function (error, response, body) {
-		        if (!error && response.statusCode == 200) {
-					
-					        	
+		        if (!error && response.statusCode == 200) {        	
 		        	var info = JSON.parse(body);
 		  			fulfill(info);
 				}
@@ -61,7 +49,6 @@ function getRequest(urlDest){
 	});	
 }
 function getRequestWithAuth(urlDest,user,pass){
-
 	return new Promise(function (fulfill, reject){
 		var authorize = 'Basic ' + new Buffer(user + ':' + pass).toString('base64');
 		request(
@@ -73,12 +60,10 @@ function getRequestWithAuth(urlDest,user,pass){
 	    },
 		    function (error, response, body) {
 		        if (!error && response.statusCode == 200) {
-		        	
 		        	var info = JSON.parse(body);
 		  			fulfill(info);
 				}
 				else{
-
 					reject(error);
 				}
 					
@@ -101,32 +86,23 @@ function size(num, text){
 function slaResults(slaObj, target){
 	var resultObj = {};
 	resultObj['overall'] = true;
-	
-
 	for(var key in slaObj){
-		if(slaObj.hasOwnProperty(key)){	
-			
+		if(slaObj.hasOwnProperty(key)){				
 			var type = key.substring(key.indexOf('(')+1, key.indexOf(')')).replace(/\s/g, '_');
 			var dataID = key.substring(key.indexOf(')')+2).replace(/\s/g,'_');
 			var min = parseFloat(slaObj[key][0]);
 			var max = parseFloat(slaObj[key][1]);
-
 			var data = null;
 			var jobResult = -1;
-
 			var displayMetric = key.substring(key.indexOf(')')+1)+': '; //display
-
 			if(type == 'System_Resources'){
 				var serverResults = [];
-
 				for(var metric in target['SingularData']['System_Resources']){
-
 					if(target['SingularData']['System_Resources'].hasOwnProperty(metric)){
 						var metricID = metric.substring(metric.indexOf(':')+1);
 						if(metricID == dataID)
 							serverResults.push(target['SingularData']['System_Resources'][metric][0]);
 					}
-
 				}
 				if(serverResults.length == 2){
 					jobResult = Math.max(parseFloat(serverResults[0]),parseFloat(serverResults[1]));
@@ -134,17 +110,14 @@ function slaResults(slaObj, target){
 				else
 					jobResult = parseFloat(serverResults[0]);
 			}
-
 			else{
 				if(target['SingularData'][type][dataID] != null){
-
 					jobResult = parseFloat(target['SingularData'][type][dataID][0]);
 				}					
 				else
 					jobResult = -1;
 			}
 			var answer = jobResult + '';
-
 			if(min <= jobResult && jobResult <= max){
 				resultObj[displayMetric] = [answer, true];
 			}
@@ -152,12 +125,10 @@ function slaResults(slaObj, target){
 					
 				if(jobResult == -1 || answer == 'NaN')
 					answer = 'Not Configured';
-
 				resultObj[displayMetric] = [answer, false];
 				resultObj['overall'] = false;
 			}
 		}
-
 	}
 	return resultObj;
 }
@@ -166,39 +137,41 @@ function getSLARequirements(jobName){
 	return getRequestWithAuth(url, username, password);
 }
 function main(){
-
 		newBuildExists().then(function(res){			
 			if(res){
 				//get build results
 				var current = fs.readFileSync('history.txt');
 				var url = "http://mule-perflab06.managed.contegix.com:8880/getParsedData?id=" + current + "&jobName=PERF_CI";
 				url = url.replace(/(\r\n|\n|\r)/gm,"");
-
+				var number = (""+current).replace(/(\r\n|\n|\r)/gm,"");
 				getRequest(url).then(function(res){
-					var perfJobs = res;	
+					var perfJobs = res;
 					// setup e-mail data with unicode symbols
 					var mailOptions = {
 					    from: 'Performance Reporter <performancereporter@mulesoft.com>', // sender address
-					    to: 'naseem.alnaji@mulesoft.com',//, wai.ip@mulesoft.com, mohammed.abouzahr@mulesoft.com', // list of receivers
-					    subject: 'Performance Report', // Subject line			    
-					    html: '<p><b>Performance Report:</b></p>' // html body
+					    to: 'naseem.alnaji@mulesoft.com', //, wai.ip@mulesoft.com, mohammed.abouzahr@mulesoft.com', // list of receivers
+					    subject: 'PERF_CI #'+number+' Report', // Subject line
+					    html: '<p><b>Performance Report for PERF_CI Run #'+number+':</b></p>' // html body
 					};
+					var timestampURL = 'http://mule-perflab06.managed.contegix.com:8080/job/PERF_CI/'+number+'/api/json?tree=timestamp';
+					getRequestWithAuth(timestampURL, username, password).then(function(res){
+						var date = new Date(parseFloat(res['timestamp']) - 28800000);
+						var timeHTML = size('18pt', (date.toUTCString()).substring(0,date.toUTCString().length-3));
+						mailOptions.html += '<p>Build completed at '+ timeHTML +'</p>';
+					});
+
 					//Store results here:
-					
 					var slaPromises = [];
 					//sla check it
 					for(var job in perfJobs){
 						if(perfJobs.hasOwnProperty(job)){
-							slaPromises.push(getSLARequirements(job))
-							
+							slaPromises.push(getSLARequirements(job))							
 						}
 					}
 					var results = [];
-					Promise.all(slaPromises).then(function(res){
-						
+					Promise.all(slaPromises).then(function(res){						
 						for(var job in perfJobs){
-							if(perfJobs.hasOwnProperty(job)){
-								
+							if(perfJobs.hasOwnProperty(job)){								
 								results.push(	{
 											  'jobName': job, 
 											  'result': slaResults(res.shift()['sla'], perfJobs[job])
@@ -207,8 +180,6 @@ function main(){
 								
 							}
 						}
-						
-						
 						for(var i=0; i<results.length; i++){
 							if(results[i]['result']['overall']){
 								mailOptions.html += size('20pt',results[i]['jobName']) + ' <b>'+colorText('green','PASSED')+'</b> the SLA Check: ';
@@ -232,7 +203,6 @@ function main(){
 							}
 							mailOptions.html += '</ul>';
 						}
-
 						//email results
 						// send mail with defined transport object
 						transporter.sendMail(mailOptions, function(error, info){
@@ -244,15 +214,10 @@ function main(){
 						        console.log('Message sent: ' + info.response);
 						        sleep(30, main);
 						    }
-
 						});
-
-					});
-										
-				});
-				
+					});										
+				});			
 			}
-
 			else{
 				sleep(30, main);
 			}
